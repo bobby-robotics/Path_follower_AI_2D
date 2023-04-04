516b8706e7e77b7d553ea89b1de30a9d6a53ce10
bobby-robotics <bobbyradev99+github@gmail.com>
2023-04-04 18:26:03 +0200

    Generator ready

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from DataGenerator.splines_generator import simple_spline
 def main():
 
     data = data_gen( 5, 5, 1280, 720)
-    data.splines_generator
+    data.splines_generator()
 
 if __name__ == '__main__':
     main()
