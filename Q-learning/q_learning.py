578d0f9c4c9b6350d86f86b4b124e7672095f8ef
bobby-robotics <bobbyradev99+github@gmail.com>
2023-05-07 20:44:07 +0200

    added some variables

---
+++ Q-learning/q_learning.py
@@ -0,0 +1,17 @@
+
+
+class q_learning():
+    
+    rewards = None
+    transition_probabilities = None
+    q_vals = None
+
+    states = None
+    actions = None
+
+    iterations = 0
+    gamma = 0
+
+    def __init__(self) -> None:
+        pass
+
