a1969f64508d0cd7a9d865f06cc31934aa81707d
bobby-robotics <bobbyradev99+github@gmail.com>
2023-05-23 09:57:50 +0200

    visualisation added and rewards edited

--- Q_learning/eviroment.py
+++ Q_learning/eviroment.py
@@ -1,30 +1,41 @@
-from enum_motion import Moutions
+from enum_motion import Motions
 import numpy as np
 # from Q_learning.state import state
 from math import sin,cos, pi, sqrt
 import cv2
 from cv2 import WINDOW_NORMAL
+import copy
+import time
 
 class enviroment():
 
     img = None
     known_tcp_pose = []
-    last_tcp_pose = None
-    state_cl = None
+    #last_tcp_pose = None
+    #state_cl = None
     img_2 = None
+    last_legit_state = None
 
 
-    def __init__(self, img, initial_tcp, state_cl) -> None:
+    def __init__(self, img, initial_tcp) -> None:
         
         self.img = img
 
-        self.last_tcp_pose = initial_tcp
+        #self.last_tcp_pose = initial_tcp
 
-        self.state_cl = state_cl
+        #state = state_cl
         self.img_2 = cv2.cvtColor(self.img*255, cv2.COLOR_GRAY2BGR)
+
+        cv2.namedWindow('tcp', WINDOW_NORMAL)
+        cv2.resizeWindow('tcp', 400,400)
+
+        cv2.namedWindow('wire', WINDOW_NORMAL)
+        cv2.resizeWindow('wire', 400,400)
+
+        cv2.namedWindow('img', WINDOW_NORMAL)
+        cv2.resizeWindow('img', 1280,720)
         
     def rotate_translate(self, temp_state, theta):
-
 
         # Rotation to be done
         # Rotation Matrix 2D
@@ -99,72 +110,106 @@ class enviroment():
 
         return temp_state
 
-    def update_state(self, action_nr):
+    def update_state(self, action_nr, state, visualise):
         
+        self.last_legit_state = copy.deepcopy(state.get_state())
+
         action_vector = self.action_nr_to_vector(action_nr)
 
-        temp_state = self.state_cl.get_state()
+        temp_state = state.get_state()
 
-        tcp_x, tcp_y = self.state_cl.get_tcp_xy()
+        tcp = state.get_tcp_xy()
 
-        self.img_2[int(tcp_y),int(tcp_x)] = (0, 0, 255)
-        cv2.imshow("img",self.img_2)
+        tcp[0] += action_vector[0]
+        tcp[1] += action_vector[1]
+
+        self.img_2[int(tcp[1]),int(tcp[0])] = (0, 0, 255)
 
         # cut a 5x5 matrix from the image
-        y1 = int(tcp_y - int(5/2)   )
-        y2 = int(tcp_y + int(5/2) +1)
-        x1 = int(tcp_x - int(5/2)   )
-        x2 = int(tcp_x + int(5/2) +1)    
+        y1 = int(tcp[1] - 2    )  #int(5/2)   
+        y2 = int(tcp[1] + 2 + 1)  #int(5/2) +1
+        x1 = int(tcp[0] - 2    )  #int(5/2)   
+        x2 = int(tcp[0] + 2 + 1)  #int(5/2) +1 
+           
         temp_state[0] = self.img[y1:y2, x1:x2]
 
         temp_state[1][2,2] += action_vector[0]
         temp_state[1][2,3] += action_vector[1] 
 
-        # cv2.namedWindow('tempStateW', WINDOW_NORMAL)
-        # cv2.resizeWindow('tempStateW', 500,500)
-        # cv2.namedWindow('tempStateTcp', WINDOW_NORMAL)
-        # cv2.resizeWindow('tempStateTcp', 500,500)
-
-        # cv2.imshow("tempStateW", temp_state[0])
-        # cv2.imshow("tempStateTcp", temp_state[1])
-        # cv2.waitKey(0)
-
         temp_state = self.rotate_translate(temp_state, action_vector[2])
 
-        return temp_state
+        if visualise:
+            cv2.imshow("wire", state.get_state()[0])
+            cv2.imshow("tcp", state.get_state()[1])
+            cv2.imshow("img",self.img_2)
+            cv2.waitKey(20)
+
+        return copy.copy(temp_state)
            
-    def colision(self):
+    def collision(self, state):
         
-        colision = False
+        collision = False
 
         # look for electodes
-        indexes_el  = np.where(self.state_cl.get_state()[1] == 1)
+        indexes_el  = np.where(state.get_state()[1] == 1)
         indexes_el = np.asarray(indexes_el).transpose()
 
         # look for wire
-        indexes_wire  = np.where(self.state_cl.get_state()[0] == 1)
-        indexes_wire = np.asarray(indexes_wire).transpose()
-
+        # indexes_wire  = np.where(state.get_state()[0] == 1)
+        # indexes_wire = np.asarray(indexes_wire).transpose()
+        #print("index Wire\n",indexes_wire)
         
 
+        # for index in indexes_el:
+        #     if index in indexes_wire:
+        #         print(index, "\n",indexes_wire)
+        #         #time.sleep(3)
+        #         colision = True
+        #         break
+
+        
+        # for index in indexes_el:
+        #     for index_w in indexes_wire:
+        #         if np.array_equal(index, index_w):
+        #             print(index, "\n",index_w)
+        #             #time.sleep(3)
+        #             colision = True
+        #             break
+
         for index in indexes_el:
-            if index in indexes_wire:
-                print(index)
-                colision = True
+            #print(state.get_state()[0][index[0],index[1]])
+            if state.get_state()[0][index[0],index[1]] == 1:
+                collision = True
                 break
+            
+        # print(state.get_state()[0], "\n", indexes_el)
+        # time.sleep(3)
 
-        return colision
+        return collision
 
-    def get_reward(self):
+    def get_reward(self, state):
+
         reward = 0
+        
+        col = self.collision(state)
 
-        if self.colision():
+        wire_det = False
+
+        wire = np.where(state.get_state()[0])
+        wire = np.asarray(wire).transpose()
+
+        if len(wire) < 4:
+            print("No Wire There!")
+            wire_det = False
+            reward -= 50
+
+        elif col:
             reward -= 10
 
         else: 
-            reward +=1
+            reward += 10
 
-        tcp_x, tcp_y = self.state_cl.get_tcp_xy()
+        tcp_x, tcp_y = state.get_tcp_xy()
 
         # if tcp_x == self.last_tcp_pose[0] and tcp_y == self.last_tcp_pose[1]:
         #     reward +=1
@@ -181,40 +226,42 @@ class enviroment():
 
         if tcp_x < 5 or tcp_x > self.img.shape[1]-5:
             reward -= 1000
+            col = True
 
         if tcp_y < 5 or tcp_y > self.img.shape[0]-5:
             reward -= 1000
+            col = True
 
-        return reward
+        return reward, col , wire_det
 
     def action_nr_to_vector(self,action_nr):
 
-        print("Action: " , Moutions(action_nr).name)
+        print("Action: " , Motions(action_nr).name)
         action_vector = np.zeros(3)
 
-        if Moutions(action_nr).name == "u":
+        if Motions(action_nr).name == "u":
 
             action_vector[1] -= 1
 
-        elif Moutions(action_nr).name == "d":
+        elif Motions(action_nr).name == "d":
 
             action_vector[1] += 1
 
-        elif Moutions(action_nr).name == "l":
+        elif Motions(action_nr).name == "l":
 
             action_vector[0] -= 1
 
 
-        elif Moutions(action_nr).name == "r":
+        elif Motions(action_nr).name == "r":
 
             action_vector[0] += 1
 
 
-        elif Moutions(action_nr).name == "w":
+        elif Motions(action_nr).name == "w":
 
             action_vector[2] -= 45
 
-        elif Moutions(action_nr).name == "c":
+        elif Motions(action_nr).name == "c":
 
             action_vector[2] += 45
 
@@ -222,5 +269,6 @@ class enviroment():
 
         return action_vector
 
-
+    def get_last_legit_state(self):
+        return self.last_legit_state
 
